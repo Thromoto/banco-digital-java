7cc23b13d75b4bbaf5fb07a7d4a2a878c2320ca2
Thiago Omoto <thi.omoto@gmail.com>
2023-10-12 19:14:09 -0300

    criando banco digital

--- src/Banco.java
+++ src/Banco.java
@@ -1,5 +1,15 @@
 public class Banco {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+		cliente.setName("Thiago");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		cc.depositar(10000);
+		cc.transferir(5000, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
